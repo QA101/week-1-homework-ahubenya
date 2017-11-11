1109a4a83b43cdaf4e11ccb70356c0525e9917e3
ahubenya <32288508+ahubenya@users.noreply.github.com>
2017-11-11 13:36:24 -0800

    find max hw

--- src/FindMax.java
+++ src/FindMax.java
@@ -1,3 +1,6 @@
+package weekTwoProject;
+
+import java.util.Scanner;
 
 public class FindMax {
 
@@ -5,7 +8,19 @@ public class FindMax {
 	 * Define a program that finds the maximum number when 2 numbers are given.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int x, y;
+		Scanner in  = new Scanner(System.in);
+		System.out.println("Enter First Number: ");
+		x = in.nextInt();
+		System.out.println("Enter Second Number: ");
+		y = in.nextInt();
+		
+		if(x > y) {
+			System.out.println("The Maximum number entered is: "+ x);
+		}
+		else
+			System.out.println("The Maximum number entered is: "+ y);
 
 	}
 
